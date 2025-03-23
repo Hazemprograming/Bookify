d51992a02514a668a4231126310df44fc6182a4b
Hazem El-Taher <hazemproductivity@gmail.com>
2025-03-23 17:55:05 +0200

    Hazem Changes 23/3

--- Bookify.Web/Core/Consts/Errors.cs
+++ Bookify.Web/Core/Consts/Errors.cs
@@ -4,5 +4,7 @@
     {
         public const string MaxLength = "Length cannot  be more than {1} charactars";
         public const string Dublicated = "{0} With the same name is already exists";
+        public const string NotAllowedExtensions = "Only .jpg .jpeg .png are allowed !";
+        public const string MaxSize = "you can not put an image more than 2MB";
     }
 }
